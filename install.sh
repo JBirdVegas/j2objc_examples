ede11b8c160d42974a2fd240d740c36fd4073ce8
Quoc Dung Chu <quocdung.chu@gmail.com>
2015-11-11 21:10:34 +0100

    Update

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ LOCAL_PROPS_FILE=$PROJECT_DIR/local.properties
 
 if [ ! -e $LOCAL_PROPS_FILE ]; then
 	touch $LOCAL_PROPS_FILE
-	echo ../common/local.properties > $LOCAL_PROPS_FILE
+	echo j2objc.home=../localJ2objcDist/j2objc-0.9.8.2.1 > $LOCAL_PROPS_FILE
 	echo "./$PROJECT_DIR/local.properties configured:"
 	cat $LOCAL_PROPS_FILE
 fi
