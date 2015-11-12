bec3b6fa66ee2747f2c17cefddd3df5a66779b03
Quoc Dung Chu <quocdung.chu@gmail.com>
2015-11-12 21:50:53 +0100

    Add test in android

--- simple/app/src/androidTest/java/com/qchu/simple/LoginInteractorSpec.java
+++ simple/app/src/androidTest/java/com/qchu/simple/LoginInteractorSpec.java
@@ -1,9 +1,35 @@
 package com.qchu.simple;
 
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.qchu.LoginInteractor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by louischu on 12/11/15.
  */
-
+@RunWith(AndroidJUnit4.class)
 public class LoginInteractorSpec {
+  LoginInteractor loginInteractor;
 
+  @Before
+  public void setup(){
+    this.loginInteractor = new LoginInteractor();
+  }
+
+  @Test
+  public void success(){
+
+    loginInteractor.createUser("tom","tom pwd");
+
+    assertThat(loginInteractor.login("tom","tom pwd"),
+      equalTo(LoginInteractor.LoginState.SUCCESS));
+  }
 }
