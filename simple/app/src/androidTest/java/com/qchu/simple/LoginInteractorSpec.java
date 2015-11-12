8ee5fb0a2a04400919edc9a89118b357970536b2
Quoc Dung Chu <quocdung.chu@gmail.com>
2015-11-12 21:41:32 +0100

    Include shared in app module

---
+++ simple/app/src/androidTest/java/com/qchu/simple/LoginInteractorSpec.java
@@ -0,0 +1,9 @@
+package com.qchu.simple;
+
+/**
+ * Created by louischu on 12/11/15.
+ */
+
+public class LoginInteractorSpec {
+
+}
--- simple/app/src/main/java/com/qchu/simple/MainActivity.java
+++ simple/app/src/main/java/com/qchu/simple/MainActivity.java
@@ -2,6 +2,9 @@ package com.qchu.simple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.qchu.LoginInteractor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    LoginInteractor loginInteractor = new LoginInteractor();
+    loginInteractor.createUser("tom", "tom pwd");
+    Log.d("login", loginInteractor.login("tom", "tom pwd").toString());
+
   }
 }
